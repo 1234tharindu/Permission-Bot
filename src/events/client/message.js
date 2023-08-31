81a4b55483ed3d3988d397f9cb11e37f51dffec2
1234Tharindu <83854250+1234Tharindu@users.noreply.github.com>
2023-08-31 15:02:16 +0530

    bh

--- src/events/client/message.js
+++ src/events/client/message.js
@@ -3,7 +3,8 @@ const { Events } = require("discord.js");
 module.exports = {
     name: Events.MessageCreate,
     once: false,
-    async execute(message) {
+    async execute(message,) {
+        console.log(message);
         // Auto Reply
             if (message.content.toLowerCase() === "hi") {
                 message.reply({ files: ['https://tenor.com/view/bad-teeth-breath-hot-gif-23600638.gif'] });
