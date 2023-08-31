3d990f2b057d4fab3e55811aa4c378ff9d158cde
1234Tharindu <83854250+1234Tharindu@users.noreply.github.com>
2023-08-31 14:55:29 +0530

    wrf

--- src/events/client/message.js
+++ src/events/client/message.js
@@ -5,7 +5,8 @@ module.exports = {
     once: false,
     async execute(message) {
         // Auto Reply
-        if (message.channel.name === "general-chat")
+            if (message.content.toLowerCase() === "hi") {
                 message.reply({ files: ['https://tenor.com/view/bad-teeth-breath-hot-gif-23600638.gif'] });
+            };
     }
 }
