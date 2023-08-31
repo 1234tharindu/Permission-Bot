0dad890520f461fe46e313468ec0735d0348014e
1234Tharindu <83854250+1234Tharindu@users.noreply.github.com>
2023-08-31 14:48:32 +0530

    as

--- src/events/client/message.js
+++ src/events/client/message.js
@@ -5,8 +5,7 @@ module.exports = {
     once: false,
     async execute(message) {
         // Auto Reply
-        console.log(message.content,"d")
-        message.channel.send(message.content)
+        message.reply("")
         if (message.channel.name === "general-chat")
             if (message.content.toLowerCase() === "hi") {
                 message.reply({ files: ['https://tenor.com/view/bad-teeth-breath-hot-gif-23600638.gif'] });
